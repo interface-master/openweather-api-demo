8ce300588e66ae5eb3eb455b5c0a490861006165
Michael Vinogradov <interface.master@gmail.com>
2024-06-14 15:22:11 -0400

    🎨 scaffolding React App component

---
+++ react/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>OpenWeather API Demo</h1>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
