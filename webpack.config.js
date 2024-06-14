f7351208681858b30d21199275ffd57922aaa943
Michael Vinogradov <interface.master@gmail.com>
2024-06-14 15:24:06 -0400

    🔧 adding webpack.config and tsconfig files

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  entry: './react/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'OpenWeather API Demo Project',
+        template: 'react/index.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'bundle.css',
+    }),
+  ],
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+
+  performance: {
+    maxAssetSize: 500000,
+    maxEntrypointSize: 500000,
+  }
+};
