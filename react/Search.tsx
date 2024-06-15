e56cb13051264506ff578e530ad1e3c2e5254b88
Michael Vinogradov <interface.master@gmail.com>
2024-06-15 10:07:45 -0400

    🔍 implementing typeahead search

--- react/Search.tsx
+++ react/Search.tsx
@@ -1,17 +1,72 @@
-import { useContext } from 'react';
+import { ChangeEvent, ReactElement, useContext, useEffect, useRef, useState } from 'react';
+import { City, ICity } from 'country-state-city';
 
 import WrapperContext from "./Wrapper.context";
 
-function Search() {
+const Search = () => {
     const { city, setCity } = useContext(WrapperContext);
+    const [ searchTerm, setSearchTerm ] = useState('' as string);
+    const [ filteredCities, setFilteredCities ] = useState([] as ICity[]);
+    const timeoutRef = useRef(undefined as NodeJS.Timeout | undefined);
+    const cities = City.getAllCities();
+    
+    const debounceSearch = (searchTerm: string) => {
+        clearTimeout(timeoutRef.current);
+
+        timeoutRef.current = setTimeout(() => {
+            if (searchTerm) {
+                const filtered = cities.filter(c => 
+                    c.name.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+                console.log('filtered:',filtered);
+                setFilteredCities(filtered);
+            } else {
+                setFilteredCities([]);
+            }
+        }, 250);
+    }
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('setting search term',event.target.value);
+        setSearchTerm(event.target.value);
+        debounceSearch(event.target.value);
+    }
+
+    const handleCitySelected = (selection: string) => {
+        console.log('selected', selection);
+        setCity(selection);
+        setSearchTerm('');
+        setFilteredCities([]);
+    }
+
+    const getFilteredCities = () => {
+        console.log('getting filteredCities',filteredCities);
+        return filteredCities.map((c,idx) => {
+            if (idx < 10) {
+                return (<li key={idx} onClick={() => handleCitySelected(c.name)}>
+                    {c.name}, {c.stateCode}, {c.countryCode}
+                </li>);
+            }
+        });
+    }
 
     return (
-        <>
+        <div className='typeahead'>
             <h1>Search</h1>
+            <input
+                type='text'
+                value={searchTerm}
+                onChange={handleInputChange}
+                placeholder='Search for any City...'
+            />
+            {filteredCities.length > 0 && (
+                <ul className='suggestions'>
+                    {getFilteredCities()}
+                </ul>
+            )}
+            
             <div>Selected City: `{city}`</div>
-            <button onClick={() => setCity('Toronto')}>Toronto</button>
-            <button onClick={() => setCity('Montreal')}>Montreal</button>
-        </>
+        </div>
     )
 }
 
