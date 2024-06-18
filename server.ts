ca59101a22f12364c37846fc1531f4324d881fd5
Michael Vinogradov <interface.master@gmail.com>
2024-06-18 14:26:36 -0400

    🚮 cleanup

--- server.ts
+++ server.ts
@@ -1,10 +1,5 @@
 import express, { Express, Request, Response } from 'express';
 import axios from 'axios';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename); 
 
 const app: Express = express();
 
